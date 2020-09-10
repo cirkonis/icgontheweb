c50dc42e958f954ea14597abc5ef1463a3f112b7
michaelmarigliano <michael@sharkcell.dk>
2020-09-10 19:37:39 +0200

    stablish

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <title>MikesHomeWork</title>
+    <img alt="Vue logo" src="./assets/mHWlogo.png">
+    <HelloWorld msg="Lucky you! You found where mike will put his homework :D"/>
+    <button class="google" v-on:click="goGoogle">Maybe find something else to do?</button>
   </div>
 </template>
 
@@ -12,6 +14,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    goGoogle:function(){
+      window.open("https://www.google.com");
+}
   }
 }
 </script>
@@ -25,4 +32,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .google{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
